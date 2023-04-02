407af5c34b6cc4ba3cc0801b25689663a5b3dd5f
William Wong <william.wai.lim.wong@gmail.com>
2023-04-02 19:41:19 -0400

    completed array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+//const record = [
+   // { year: "2015", result: "W"},
+   // { year: "2014", result: "N/A"},
+   // { year: "2013", result: "L"},
+    //...
+ // ]
+
+  function superbowlWin(array){ 
+    let winYear = array.find(winLoss=> winLoss.result === "W");
+      if (winYear){
+        return winYear.year;
+      }
+      else {
+        return undefined;
+      } 
+  }
